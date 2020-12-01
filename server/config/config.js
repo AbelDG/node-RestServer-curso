71f791c5b2bc57fd0fb42c97e9fd59c1a26f64b0
Abel <abelibi95@gmail.com>
2020-12-01 20:09:40 +0100

    Google Sign In

--- server/config/config.js
+++ server/config/config.js
@@ -34,13 +34,14 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 // Base de datos
 // ========================================
 let urlDB;
+
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URL;
 }
 
-
+console.log(process.env.NODE_ENV);
 console.log(`Conectado a la base de datos ${color.yellow(urlDB)}`);
 process.env.URLDB = urlDB;
 
